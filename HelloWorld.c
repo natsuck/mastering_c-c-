239440412f6076b2bd79f960627459ab0db253ca
Nath <nathanielquirino3210@gmail.com>
2025-02-22 22:53:19 +0800

    third commit studying variables

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    //single line comment
+    /*Multi 
+    line
+    comment
+    */
     printf("Hello World!\n");
-    printf("I love Mira so much");
+    printf("I\nlove\nMira\nso much\n");
+    /*escape sequence
+    \n = newline
+    \t = new tab
+    \"quote\" = quote 
+    */
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
 
     return 0;
 }
---
+++ variable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int x; //declaration
+    x = 21; //initialization
+    int y = 20; // declaration & initialization
+
+    int age = 22;
+    float weight = 72.5;
+    char faveletter = 'm';
+    char name[] = "Mira";
+
+    printf("You are &d years old.",age);
+    return 0;
+}
