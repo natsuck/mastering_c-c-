755071196644f772fa6706d96501914d4ee3ffc6
Nath <nathanielquirino3210@gmail.com>
2025-02-25 13:14:53 +0800

    constant in c

---
+++ constant.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    //const = constant, fixed value that cannot be altered or changed by the program during execution.
+    const float pi = 3.14159;
+
+
+    printf("%f", pi);
+
+    return 0;
+}
